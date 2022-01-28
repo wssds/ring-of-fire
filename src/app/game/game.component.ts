7bca40065b66e35157cb1f5cf63a2ad965f9a9c1
wssds Sn Gk <ssean.gerigk@gmx.net>
2022-01-28 11:02:19 +0100

    shuffle

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game: Game | undefined;
+ 
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard(){
